a128183f68a41f1fc5503b0dfb5dd73452975c4e
Mihir Patil <mihir.patil@berkeley.edu>
2014-11-30 01:05:39 -0800

    Fixed 2^2 bug in tests by making it 2**2, like it should be. All tests pass.

--- XorReduction/tests.py
+++ XorReduction/tests.py
@@ -9,95 +9,86 @@ num_correct	= 0			# counter for the total number of tests passed
 
 
 def run_tests():
-	arr = (np.ones(2^0) * 8).astype(np.float32)
+	arr = (np.ones(2**0) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^1) * 8).astype(np.float32)
+	arr = (np.ones(2**1) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
 	# FIXME: This tests fails
-	arr = (np.ones(2^2) * 8).astype(np.float32)
+	arr = (np.ones(2**2) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^3) * 8).astype(np.float32)
+	arr = (np.ones(2**3) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^4) * 8).astype(np.float32)
+	arr = (np.ones(2**4) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^5) * 8).astype(np.float32)
+	arr = (np.ones(2**5) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^6) * 8).astype(np.float32)
+	arr = (np.ones(2**6) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^7) * 8).astype(np.float32)
+	arr = (np.ones(2**7) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^8) * 8).astype(np.float32)
+	arr = (np.ones(2**8) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^9) * 8).astype(np.float32)
+	arr = (np.ones(2**9) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^10) * 8).astype(np.float32)
+	arr = (np.ones(2**10) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^11) * 8).astype(np.float32)
+	arr = (np.ones(2**11) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^12) * 8).astype(np.float32)
+	arr = (np.ones(2**12) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^13) * 8).astype(np.float32)
+	arr = (np.ones(2**13) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^14) * 8).astype(np.float32)
+	arr = (np.ones(2**14) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^15) * 8).astype(np.float32)
+	arr = (np.ones(2**15) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^16) * 8).astype(np.float32)
+	arr = (np.ones(2**16) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^17) * 8).astype(np.float32)
+	arr = (np.ones(2**17) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^19) * 8).astype(np.float32)
+	arr = (np.ones(2**19) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^20) * 8).astype(np.float32)
+	arr = (np.ones(2**20) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^21) * 8).astype(np.float32)
+	arr = (np.ones(2**21) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^22) * 8).astype(np.float32)
+	arr = (np.ones(2**22) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^23) * 8).astype(np.float32)
+	arr = (np.ones(2**23) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^24) * 8).astype(np.float32)
+	arr = (np.ones(2**24) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^25) * 8).astype(np.float32)
+	arr = (np.ones(2**25) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^26) * 8).astype(np.float32)
+	arr = (np.ones(2**26) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
-	arr = (np.ones(2^27) * 8).astype(np.float32)
-	check_test(do_test(arr), do_control(arr))
-
-	arr = (np.ones(2^28) * 8).astype(np.float32)
-	check_test(do_test(arr), do_control(arr))
-
-	arr = (np.ones(2^29) * 8).astype(np.float32)
-	check_test(do_test(arr), do_control(arr))
-
-	arr = (np.ones(2^30) * 8).astype(np.float32)
+	arr = (np.ones(2**27) * 8).astype(np.float32)
 	check_test(do_test(arr), do_control(arr))
 
 def do_test(arr, work_group_size=None, target_gpu_index=0):
